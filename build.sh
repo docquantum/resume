46dc2fa4ec5412af31c4673b470ec84653dcaa8c
DocQuantum <shchurgood@gmail.com>
2024-09-07 22:43:54 -0500

    Improve build process

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-gpp -H -DPUBLIC -o pub.md resume.md &&
-gpp -H -o priv.md resume.md &&
-pandoc -s pub.md -o resume-public.html --template=template.html &&
-pandoc -s priv.md -o resume-pdf.html --template=template-pdf.html -c basicstyle.css &&
-chromium --headless --disable-gpu --print-to-pdf=resume-private.pdf --virtual-time-budget=10000 resume-pdf.html
-rm pub.md priv.md resume-pdf.html
+# Run resume through preprocessors
+gpp -H -DPUBLIC -o build/pub.md resume.md &&
+gpp -H -o build/priv.md resume.md &&
+
+# Convert to HTML
+pandoc -s build/pub.md -o build/resume-public.html --template=template.html &&
+pandoc -s build/priv.md -o build/resume-pdf.html --template=template-pdf.html &&
+
+# Use chrome to print to pdf
+chromium --headless --disable-gpu --print-to-pdf=build/resume-private.pdf --virtual-time-budget=10000 build/resume-pdf.html
+
+# Clean up build dir
+rm build/pub.md build/priv.md build/resume-pdf.html
